0e208700a0c7cc25c1b5aa75cec5b06cbeee91bc
kshitijgupta468035 <kshitij.gupta1_cs18@gla.ac.in>
2020-03-01 21:58:15 +0530

    print binaryString

--- src/main/Main.java
+++ src/main/Main.java
@@ -24,6 +24,7 @@ public class Main {
         int lengthOfBinaryString = binaryString.length();
         int lengthOfHuffmanString = huffmanString.length();
         System.out.println("The binary string has " + lengthOfBinaryString + " characters.");
+        System.out.println(binaryString);
         System.out.println("The huffman string has " + lengthOfHuffmanString + " characters.");
         System.out.println("Compression Ratio (Huffman/Binary): " + (lengthOfHuffmanString / lengthOfBinaryString) + "%");
         scanner.close();
